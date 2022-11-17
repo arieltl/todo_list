985f6de269967bf4e20d35d0828770529e587037
Antônio A E Martins <antonioaem@icloud.com>
2022-11-17 18:27:38 -0300

    Adição LinearGradient

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -18,37 +18,53 @@ class _CreateTodoPageState extends State<CreateTodoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Create"),
+      appBar: AppBar(
       actions: [
         IconButton(onPressed: (){_signOut();}, icon: const Icon(Icons.logout))
       ],),
-      body: Center(child: Container(
-        margin: const EdgeInsets.all(20),
-        constraints: const BoxConstraints(maxHeight: 400, maxWidth: 500),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
-          // ignore: prefer_const_literals_to_create_immutables
-          boxShadow: [const BoxShadow(blurRadius: 10,color: Color.fromARGB(255, 109, 109, 109), offset: Offset(2, 2))]
-        ),
-        padding: const EdgeInsets.all(40),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            TextField(
-              controller: _controller,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Title"
-              ),
+      body: Stack(
+        fit: StackFit.passthrough,
+        children: <Widget>[
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromARGB(150, 176, 55, 206),
+                  Color.fromARGB(150, 31, 61, 171),
+                ]
+              )
             ),
-            SizedBox(
-              height: 50,
-              width: 100,
-              child: ElevatedButton(onPressed: (){}, child: const Text("Create")))
-          ],
-        ),
-      )),
+          ),
+          Center(child: Container(
+            margin: const EdgeInsets.all(20),
+            constraints: const BoxConstraints(maxHeight: 400, maxWidth: 500),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: const [BoxShadow(blurRadius: 10,color: Color.fromARGB(255, 109, 109, 109), offset: Offset(2, 2))]
+            ),
+            padding: const EdgeInsets.all(40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextField(
+                  controller: _controller,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Title"
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                  width: 100,
+                  child: ElevatedButton(onPressed: (){}, child: const Text("Create")))
+              ],
+            ),
+          )),
+        ]
+      ),
     );
   }
 }
